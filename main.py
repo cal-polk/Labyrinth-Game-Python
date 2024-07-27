b1bce876f1ef7041250a8a9c1acc6b10cf6a8046
Beatrice <beatricepolk504@gmail.com>
2024-07-26 23:05:50 -0400

    Add files via upload
    Added "main" method to begin processing commands as entered by user in the starting room of the labyrinth game. Added "clean" method to add uniformity to all commands entered by user -- all commands are made lowercase, extra spaces are removed, and valid commands are abbreviated to their first letter. Current valid commands are "north", "south", "east", and "examine". Added "examine" method to begin list of targets that can be used with that method. Noted the necessity of a location tracker but have not yet implemented one. Need to add the paper list to "examine" method.

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("Hello World")
+def clean(command_string):
+      command_string = command_string.strip();
+      command_string = command_string.lower();
+      if (command_string == "south"):
+            return "s"
+      elif (command_string == "west"):
+            return "w"
+      elif (command_string == "east"):
+            return "e"
+      elif (command_string == "north"):
+            return "n"
+      elif (command_string.find("examine") == 0):
+            command_string = command_string.replace("examine", "e")
+            return command_string
+      else:
+            return command_string
+      
+def examine(command_string):
+      item = command_string[2:]
+      if (item == "rug"): #need to verify the item is actually in sight. Some kind of location tracker -- object and set method?
+            print("The rug is faded and dusty. It appears as though it could be moved.")
+
+def main():
+      print('You are standing in the middle of a stone room on top of a rug. To the north, south, east and west', 
+      'there are windows.')
+      command = input(">")
+      command = clean(command)
+      if (command == "s" or command == "e" or command == 'n'):
+            print("You are standing at a window overlooking a large maze")
+      elif (command == "w"):
+            print("You are standing at a window overlooking a large maze. A gateway out can be seen in the distance. On the windowsill is a slip of paper.")
+      elif (command.find("e") == 0):
+            examine(command)
+      else :
+            print("That command could not be recognized")
+
+if __name__ =="__main__":
+      main()
+
+
+
