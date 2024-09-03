cca3cff33efde2ec74dfa6df105841e0c7027168
Beatrice <beatricepolk504@gmail.com>
2024-09-02 23:05:23 -0400

    Starting List
    Added the items in the list found in the starting room as a string within the examine function. Realized that the location name will be impossible to access without an instance of the object which cannot be shared across main and examine functions -- troubleshoot this. Added a variable that notates the end of the "while" loop that keeps prompting users for inputs.

--- main.py
+++ main.py
@@ -26,25 +26,30 @@ def clean(command_string):
             return command_string
       
 def examine(command_string):
-      item = command_string[2:]
+      item = command_string[3:]
       if (item == "rug"): #need to verify the item is actually in sight. Some kind of location tracker -- object and set method?
             print("The rug is faded and dusty. It appears as though it could be moved.")
       elif (item == "slip of paper"):
-            print("The slip of paper contains a list of the following items:")
+            print("The slip of paper contains a list of the following items:\nBeethoven\nEmily Dickinson\nHoundstooth\n",
+                  "Chicago\ne^(ix)\n7\nNarcissus\nHammurabi\nPan\nKhufu\nCatfish\nApollo 13\nBram Stoker")
 
 def main():
       print('You are standing in the middle of a stone room on top of a rug. To the north, south, east and west', 
       'there are windows.')
-      command = input(">")
-      command = clean(command)
-      if (command == "s" or command == "e" or command == 'n'):
-            print("You are standing at a window overlooking a large maze")
-      elif (command == "w"):
-            print("You are standing at a window overlooking a large maze. A gateway out can be seen in the distance. On the windowsill is a slip of paper.")
-      elif (command.find("ex") == 0):
-            examine(command)
-      else :
-            print("That command could not be recognized")
+      isEnd = False
+      while(not isEnd):
+            command = input(">")
+            command = clean(command)
+            if (command == "s" or command == "e" or command == 'n'):
+                  print("You are standing at a window overlooking a large maze")
+            elif (command == "w"):
+                  print("You are standing at a window overlooking a large maze. A gateway out can be seen in the distance. On the windowsill is a slip of paper.")
+            elif (command.find("ex") == 0):
+                  examine(command)
+            else :
+                  print("That command could not be recognized")
+                  isEnd = True
+
 
 if __name__ =="__main__":
       main()
